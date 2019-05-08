8d9c4c013735c3ba8267e1379ab83d3de047e9e4
Andri <andri.wakhid@mii.co.id>
2019-05-08 14:43:38 +0700

    commit

---
+++ CRUDTest/Controllers/EmployeesController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUDTest.Models;
+
+namespace CRUDTest.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        /// READ
+        public ActionResult Index()
+        {
+            var x = db.Employees.ToList();
+
+            //return Json(new { Data = x }, JsonRequestBehavior.AllowGet);
+            return View(x);
+        }
+
+        /// CREATE
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public JsonResult Save(string FName, string LName, string Phone, string Address)
+        {
+            bool status = false;
+            try
+            {
+                //define the model  
+                if (FName == "")
+                {
+                    status = false;
+                }
+                else
+                {
+                    Employee data = new Employee();
+                    data.FirstName = FName;
+                    data.LastName = LName;
+                    data.Phone = Phone;
+                    data.Address = Address;
+
+                    db.Employees.Add(data);
+                    status = true;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.Write(ex.InnerException);
+                status = false;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+
+        /// UPDATE
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+            //Employee employee = db.Employees.Where(x => x.Id == id).SingleOrDefault();
+            //return Json(new { Data = employee }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Update(int Id, string FName, string LName, string Phone, string Address)
+        {
+            bool status = false;
+            try
+            {
+                if (FName == "")
+                {
+                    status = false;
+                }
+                else
+                {
+                    //define the model  
+                    var data = db.Employees.Where(x => x.Id.Equals(Id)).SingleOrDefault();
+                    data.FirstName = FName;
+                    data.LastName = LName;
+                    data.Phone = Phone;
+                    data.Address = Address;
+
+                    db.Entry(data).State = EntityState.Modified;
+                    status = true;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.Write(ex.InnerException);
+                status = false;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        /// DELETE
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+        public JsonResult Del(int Id)
+        {
+            bool status = false;
+            try
+            {
+                //define the model  
+                Employee data = db.Employees.Find(Id);
+
+                db.Employees.Remove(data);
+                status = true;
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                System.Console.Write(ex.InnerException);
+                status = false;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
---
+++ CRUDTest/Scripts/MyScript.js
@@ -0,0 +1,105 @@
+﻿$(document).ready(function () {
+
+    $("#btnSave").click(function () {
+        var data = {
+            FName: $("#txtFName").val(),
+            LName: $("#txtLName").val(),
+            Phone: $("#txtPhone").val(),
+            Address: $("#txtAddress").val()
+        }
+        $.ajax({
+            type: 'POST',
+            url: '/Employees/Save',
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                if (data.status == true) {
+                    alert('Successful save');
+                    clear();
+                    window.location.href = "Index";
+                }
+                else {
+                    alert('First Name is required');
+                    $("#txtFName").val("");
+                }
+            }
+        });
+    });
+
+    $("#btnUpdate").click(function () {
+        var data = {
+            Id: $("#EtxtId").val(),
+            FName: $("#EtxtFName").val(),
+            LName: $("#EtxtLName").val(),
+            Phone: $("#EtxtPhone").val(),
+            Address: $("#EtxtAddress").val()
+        }
+        $.ajax({
+            type: 'POST',
+            url: '/Employees/Update',
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                if (data.status == true) {
+                    alert('Successful update');
+                    clear();
+                    window.location.href = "../Index/";
+                }
+                else {
+                    alert('First Name is required');
+                    $("#EtxtFName").val("");
+                }
+            }
+        });
+    });
+
+    $("#btnDelete").click(function () {
+        var data = {
+            Id: $("#DtxtId").val()
+        }
+        $.ajax({
+            type: 'POST',
+            url: '/Employees/Del',
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                if (data.status == true) {
+                    alert('Successful delete');
+                    clear();
+                    window.location.href = "../Index/";
+                }
+                else {
+                    alert('Error');
+                    clear();
+                    window.location.href = "../Index/";
+                }
+            }
+        });
+    });
+
+
+    $("#btnCancel").click(function () {
+        clear();
+        window.location.href = "Index";
+    });
+    $("#UbtnCancel").click(function () {
+        clear();
+        window.location.href = "../Index";
+    });
+    $("#DbtnCancel").click(function () {
+        clear();
+        window.location.href = "../Index";
+    });
+});
+
+function clear() {
+    $("#txtFName").val("");
+    $("#txtLName").val("");
+    $("#txtPhone").val("");
+    $("#txtAddress").val("");
+    $('#btnSave').val("Save");
+    $("#hdnEId").val(0);
+}
